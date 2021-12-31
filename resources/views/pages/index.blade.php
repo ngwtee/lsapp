7669ec06b146a8e0379f964e7f1961cbca0cbc21
Ngoni <ngonidzashegwt@gmail.com>
2021-12-31 04:24:19 +0200

    views and controllers set up

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{config('app.name', 'LSAPP')}}</title>
+</head>
+<body>
+    <h1> Hello on index</h1>
+    <p>This is the laravel series</p>
+</body>
+</html>
